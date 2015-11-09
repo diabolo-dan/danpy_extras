599eed56396e5e3aa2b0e54de2b512765125cdcf
diabolo-dan <diabolo.dan@gmail.com>
2015-11-09 19:35:05 +0000

    argument_dict -- add a __repr__ to avoid confusion

--- argument_dict.py
+++ argument_dict.py
@@ -25,3 +25,6 @@ class ArgumentativeDict(defaultdict):
 			raise KeyError(key)
 		self[key] = value = self.default_factory(key)
 		return value
+
+	def __repr__(self):
+		return "<ArgumentativeDict(%s.%s)>: %r" % (self.default_factory.__module__, self.default_factory.__name__, self.__dict__)
