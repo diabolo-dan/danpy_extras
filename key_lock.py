f037cd1c376612480b892bb72371276b0f2ada1c
diabolo-dan <diabolo.dan@gmail.com>
2015-11-09 20:09:35 +0000

    key_lock -- add docstring explaining KeyLock

--- key_lock.py
+++ key_lock.py
@@ -3,6 +3,22 @@ from contextlib import contextmanager
 from collections import defaultdict
 
 class KeyLock(object):
+	"""
+	A collection of Key-seperated Locks in the form of context_managers.
+
+	For use with dynamically created resources that are used for an extended period.
+
+	Use as:
+		keyed_lock = KeyLock()
+		...
+		with keyed_lock(my_key):
+			long_running_resource_use(my_key)
+
+	Be aware that this has extra overhead compared to just using a single Lock, so will be slower for many use-cases.
+
+	A good use case would be taking a Lock on a resource while waiting for a response to an external request.
+	"""
+
 	def __init__(self):
 		self._glock = Lock()
 		self._lock_for_key = defaultdict(Lock)
